5224af9c85eb2f88be6dabf9015f2149e9abbdc9
James Zackary <jmwihia@gmail.com>
2021-03-29 11:23:54 -0400

    appsettings ignore confirmation

---
+++ Testing/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Testing.Models
+{
+
+    /// <summary>
+    /// Summary description for Class1
+    /// </summary>
+    public class Product
+    {
+        public Product()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CategoryID { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+    }
+}
